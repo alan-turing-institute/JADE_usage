82ba71e33c25f05fad53ccd11dc92b381d1e35e9
Jim Madge <jmmadge@gmail.com>
2019-12-13 11:21:20 +0000

    Add docstrings

--- jade_usage/export.py
+++ jade_usage/export.py
@@ -7,6 +7,19 @@ JADE_ADDRESS = "jade.hartree.stfc.ac.uk"
 
 
 def fetch(user, start_date, end_date):
+    """
+    Fetch usage data from JADE using the 'sacct' command over SSH.
+
+    Args:
+        user (str): The username to attempt to login as.
+        start_date (str): The earliest date to get usage for in the format
+            YYYY-MM-DD.
+        end_date (str): The latest date to get usage for in the format
+            YYYY-MM-DD.
+
+    Returns:
+        (str): The output of sacct as a string.
+    """
     result = run(["ssh",
                   "{user}@{jade}".format(user=user, jade=JADE_ADDRESS),
                   "sacct",
@@ -25,6 +38,19 @@ def fetch(user, start_date, end_date):
 
 
 def export(user, start_date, end_date):
+    """
+    Export usage data from JADE to a csv file. The data is written to a file
+    named {start_date}-{end_date}_usage.csv
+
+    This function uses fetch.
+
+    Args:
+        user (str): The username to attempt to login as.
+        start_date (str): The earliest date to get usage for in the format
+            YYYY-MM-DD.
+        end_date (str): The latest date to get usage for in the format
+            YYYY-MM-DD.
+    """
     filename = "{}-{}_usage.csv".format(start_date, end_date)
 
     with open(filename, "w") as outfile:
@@ -32,6 +58,9 @@ def export(user, start_date, end_date):
 
 
 class FetchError(Exception):
+    """
+    Exception raised when fetch fails
+    """
     def __init__(self, result):
         message = (
             "Non zero return code when attempting to execute sacct over ssh\n"
