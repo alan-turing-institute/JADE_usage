dbd3ff22e15b05af096867dfd78e6e5390cbbf68
Jim Madge <jmmadge@gmail.com>
2019-12-13 10:58:39 +0000

    Add setuptools script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jade-usage",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["pandas"],
+    entry_points={
+        'console_scripts': [
+            'jade-usage = jade_usage.__main__:main'
+            ]
+        }
+    )
