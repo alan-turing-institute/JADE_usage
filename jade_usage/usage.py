f135d008ec686618ea3bcf76655cb4251898db38
Jim Madge <jmmadge@gmail.com>
2020-01-06 13:51:32 +0000

    Add usage table breakdown by group and account

--- jade_usage/usage.py
+++ jade_usage/usage.py
@@ -14,6 +14,29 @@ def _gpu_hours(df):
     return GPU_hours
 
 
+def _get_group(username):
+    """
+    Get the group from a username
+
+    Usernames are in the format <initials>-<group>. The group names are
+    therefore extracted by splitting the usernames about the hyphen.
+    """
+
+    return username.split("-")[-1]
+
+
+def _get_groups(users, df):
+    """
+    Get a unique list of groups from usernames and tag records with their
+    group
+    """
+    groups = [_get_group(elem) for elem in users]
+    groups = list(set(groups))
+
+    df = df.assign(Group=df.User.apply(_get_group))
+    return groups, df
+
+
 def usage(df, accounts=None, users=None, export=None):
     """
     Determine usage from the data in a DataFrame
@@ -36,6 +59,8 @@ def usage(df, accounts=None, users=None, export=None):
     # Filter by accounts
     if accounts:
         usage = usage[usage.Account.isin(accounts)]
+    else:
+        accounts = list(usage.Account.unique())
 
     # Filter by user names
     if users:
@@ -60,11 +85,36 @@ def usage(df, accounts=None, users=None, export=None):
         gpu_hours_user = _gpu_hours(usage[usage.User == user])
         user_df.append((user, gpu_hours_user))
     user_df = pd.DataFrame(user_df, columns=["user", "usage/GPUh"])
+    user_df.sort_values("usage/GPUh", ascending=False, inplace=True)
+
+    # Get GPU hours per group
+    groups, usage = _get_groups(users, usage)
+    group_df = []
+    for group in groups:
+        gpu_hours_group = _gpu_hours(usage[usage.Group == group])
+        group_df.append((group, gpu_hours_group))
+    group_df = pd.DataFrame(group_df, columns=["group", "usage/GPUh"])
+    group_df.sort_values("usage/GPUh", ascending=False, inplace=True)
+
+    # Get GPU hours per account
+    account_df = []
+    for account in accounts:
+        gpu_hours_account = _gpu_hours(usage[usage.Account == account])
+        account_df.append((account, gpu_hours_account))
+    account_df = pd.DataFrame(account_df, columns=["account", "usage/GPUh"])
+    account_df.sort_values("usage/GPUh", ascending=False, inplace=True)
 
     # Write human readable summary to stdout
     print(tabulate(user_df, headers="keys", showindex=False,
                    tablefmt="github"))
-    print("All users GPU hours: {:<,.2f}".format(gpu_hours))
+    print("\n")
+    print(tabulate(group_df, headers="keys", showindex=False,
+                   tablefmt="github"))
+    print("\n")
+    print(tabulate(account_df, headers="keys", showindex=False,
+                   tablefmt="github"))
+    print("\n")
+    print("Total selected GPU hours: {:<,.2f}".format(gpu_hours))
     print("Total JADE GPU hours: {:<,.2f}".format(gpu_hours_total))
 
     # If seleced, write data to a file
